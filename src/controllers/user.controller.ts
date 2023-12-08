991244ca9dcd662481ac65e12d05477a81cc44b0
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-12-08 10:24:14 +0100

    Update package json with build render

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { User } from '../entities/user';
 import { Controller } from './controller.js';
-import { UserMongoRepo } from '../repos/users/users.mongo.repo';
+import { UserMongoRepo } from '../repos/users/users.mongo.repo.js';
 import createDebug from 'debug';
+import { Auth } from '../services/auth.js';
 
 const debug = createDebug('W9E:user:controller');
 
@@ -14,10 +15,20 @@ export class UserController extends Controller<User> {
 
   async login(req: Request, res: Response, next: NextFunction) {
     try {
-      const result = await this.repo.login(req.body);
-      res.status(204);
-      /* Res.statusMessage = 'Accepted'; */
-      res.json(result);
+      const result = req.body.userId
+        ? await this.repo.getById(req.body.userId)
+        : await this.repo.login(req.body);
+      const data = {
+        user: result,
+        token: Auth.signJWT({
+          id: result.id,
+          email: result.email,
+        }),
+      };
+      debug('login controller', data);
+      res.status(202);
+      res.statusMessage = 'Accepted';
+      res.json(data);
     } catch (error) {
       next(error);
     }
