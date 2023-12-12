ce4926540a474731534159efc726f5d117fa9fb1
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-12-12 19:27:24 +0100

    2nd try to fix recipe mongo test

--- src/repos/recipes/recipes.mongo.repo.test.ts
+++ src/repos/recipes/recipes.mongo.repo.test.ts
@@ -1,4 +1,86 @@
+import { Recipe } from '../../entities/recipe';
+import { recipeModel } from './recipes.mongo.model';
 import { RecipesMongoRepo } from '../recipes/recipes.mongo.repo';
+
+jest.mock('./recipes.mongo.model');
+
+describe('Given the class RecipeMongoRepo', () => {
+  let repo: RecipesMongoRepo;
+
+  beforeEach(() => {
+    repo = new RecipesMongoRepo();
+  });
+
+  describe('When it is instantiated and its methods are called', () => {
+    test('Then, in getAll()', async () => {
+      const mockExec = jest.fn().mockResolvedValueOnce([]);
+      recipeModel.find = jest.fn().mockReturnValueOnce({
+        populate: jest.fn().mockReturnValue({
+          exec: mockExec,
+        }),
+      });
+      const result = await repo.getAll();
+      expect(mockExec).toHaveBeenCalled();
+      expect(result).toEqual([]);
+    });
+
+    test('Then, in getById()', async () => {
+      const execMock = jest.fn().mockResolvedValueOnce([]);
+      (recipeModel.findById as jest.Mock).mockReturnValueOnce({
+        populate: jest.fn().mockReturnValueOnce({
+          exec: execMock,
+        }),
+      });
+      const data = await repo.getById('id');
+      expect(execMock).toHaveBeenCalled();
+      expect(data).toEqual([]);
+    });
+    test('Then, when the method create() is called', async () => {
+      const mockRecipe = {
+        surface: '',
+      } as unknown as Recipe;
+      recipeModel.create = jest.fn().mockReturnValueOnce(mockRecipe);
+      const newRecipe = await repo.create(mockRecipe);
+
+      expect(newRecipe).toEqual(mockRecipe);
+    });
+    test('Then, when the method update() is called', async () => {
+      const mockExec = jest.fn().mockResolvedValueOnce([]);
+      (recipeModel.findByIdAndUpdate as jest.Mock).mockReturnValueOnce({
+        populate: jest.fn().mockReturnValueOnce({
+          exec: mockExec,
+        }),
+      });
+      const updatedRecipe = await repo.update('', {});
+      expect(mockExec).toHaveBeenCalled();
+      expect(updatedRecipe).toEqual([]);
+    });
+    test('Then, when the method update() didnt found the user', () => {
+      const execMock = jest.fn().mockResolvedValueOnce(null);
+      recipeModel.findByIdAndUpdate = jest.fn().mockReturnValueOnce({
+        exec: execMock,
+      });
+      expect(repo.update('', {})).rejects.toThrow();
+    });
+    test('Then, when the method delete() is called', async () => {
+      const mockExec = jest.fn().mockReturnValueOnce({});
+      recipeModel.findByIdAndDelete = jest.fn().mockReturnValueOnce({
+        exec: mockExec,
+      });
+      const result = await repo.delete('');
+      expect(result).toBe(undefined);
+    });
+    test('Then, when the method delete didnt find an user', () => {
+      const execMock = jest.fn().mockReturnValueOnce(null);
+      recipeModel.findByIdAndDelete = jest.fn().mockReturnValueOnce({
+        exec: execMock,
+      });
+      expect(repo.delete('')).rejects.toThrow();
+    });
+  });
+});
+
+/* Import { RecipesMongoRepo } from '../recipes/recipes.mongo.repo';
 import { HttpError } from '../../types/http.error';
 import { recipeModel } from './recipes.mongo.model';
 import { UserMongoRepo } from '../users/users.mongo.repo';
@@ -160,3 +242,4 @@ describe('RecipesMongoRepo', () => {
     });
   });
 });
+ */
